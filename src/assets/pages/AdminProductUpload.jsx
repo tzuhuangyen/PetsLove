e9ccc3f3580b3bf490ece5b834adaa8920ee36f5
tzuhuangyen <912yan@gmail.com>
2024-07-31 19:19:38 +0200

    add product

--- src/assets/pages/AdminProductUpload.jsx
+++ src/assets/pages/AdminProductUpload.jsx
@@ -1,40 +1,53 @@
 import { backendUrl } from '../../../config.js';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container, Row, Col, Form, Button, Table } from 'react-bootstrap';
+import { Row, Col, Form, Button } from 'react-bootstrap';
 import ProductTable from './component/ProductTable';
 import { IoCloudUploadOutline } from 'react-icons/io5';
 import Swal from 'sweetalert2';
-// const MySwal = withReactContent(Swal);
-// MySwal.fire({
-//   title: <p>Hello World</p>,
-//   didOpen: () => {
-//     // `MySwal` is a subclass of `Swal` with all the same instance & static methods
-//     MySwal.showLoading();
-//   },
-// }).then(() => {
-//   return MySwal.fire(<p>Shorthand works too</p>);
-// });
 
 function AdminProductUpload() {
   // const [file, setFile] = useState(null); // 追加 file 状态来存储选择的文件
   const [image, setImage] = useState(null);
   const [allImage, setAllImage] = useState(null);
-  const [productName, setProductName] = useState('');
-  const [type, setType] = useState('');
-  const [order, setOrder] = useState('');
-  const [price, setPrice] = useState('');
+  // const [productName, setProductName] = useState('');
+  // const [type, setType] = useState('');
+  // const [order, setOrder] = useState('');
+  // const [price, setPrice] = useState('');
+  const [newProductData, setNewProductData] = useState({
+    productName: '',
+    type: '',
+    order: '',
+    price: '',
+    is_enabled: 1,
+    imageUrl: '',
+  });
+  const handleChange = (e) => {
+    // console.log(e.target);
+    const { value, name } = e.target;
+    if (['price'].includes(name)) {
+      setNewProductData({ ...newProductData, [name]: Number(value) });
+    } else if (name === 'is_enabled') {
+      setNewProductData({
+        ...newProductData,
+        [name]: e.target.checked ? 1 : 0,
+      });
+    } else {
+      setNewProductData({ ...newProductData, [name]: value });
+    }
+  };
+
   //handle upload
   const uploadImage = async (e) => {
     e.preventDefault();
 
     const formData = new FormData();
     formData.append('image', image);
-    formData.append('productName', productName);
-    formData.append('type', type);
-    formData.append('order', order);
-    formData.append('price', price);
+    Object.keys(newProductData).forEach((key) => {
+      formData.append(key, newProductData[key]);
+    });
     console.log('FormData:', formData);
+
     try {
       const result = await axios.post(
         `${backendUrl}/api/admin/products/uploadProduct`,
@@ -47,8 +60,10 @@ function AdminProductUpload() {
       );
       console.log('Upload result:', result.data);
       getAllImage();
+      Swal.fire('Success', 'Product uploaded successfully', 'success');
     } catch (error) {
       console.error('Error uploading image:', error);
+      Swal.fire('Error', 'Failed to upload product', 'error');
     }
   };
   //get all image products
@@ -71,6 +86,7 @@ function AdminProductUpload() {
     <Col md={9}>
       <Form onSubmit={uploadImage}>
         <h1 className='text-center mb-5'>Add Product</h1>
+        {/* <pre>{JSON.stringify(newProductData)}</pre> */}
         <input
           type='file'
           className='form-control'
@@ -81,33 +97,50 @@ function AdminProductUpload() {
           <Form.Label>product Name</Form.Label>
           <Form.Control
             type='text'
-            value={productName}
-            onChange={(e) => setProductName(e.target.value)}
+            name='productName'
+            value={newProductData.productName}
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Group>
           <Form.Label>Type</Form.Label>
           <Form.Control
             type='text'
-            value={type}
-            onChange={(e) => setType(e.target.value)}
+            name='type'
+            value={newProductData.type}
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Group>
           <Form.Label>Order</Form.Label>
           <Form.Control
             type='text'
-            value={order}
-            onChange={(e) => setOrder(e.target.value)}
+            name='order'
+            value={newProductData.order}
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Group>
           <Form.Label>Price</Form.Label>
           <Form.Control
             type='text'
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
+            name='price'
+            value={newProductData.price}
+            onChange={handleChange}
           />
+          <div className='form-check mt-2 mb-2'>
+            <label className='w-100 form-check-label' htmlFor='is_enabled'>
+              active
+              <input
+                type='checkbox'
+                id='is_enabled'
+                name='is_enabled'
+                className='form-check-input'
+                onChange={handleChange}
+                checked={newProductData.is_enabled === 1}
+              />
+            </label>
+          </div>
         </Form.Group>
         <Button type='submit'>
           <IoCloudUploadOutline />
